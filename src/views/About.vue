d71562d204e5acf4c81fc5f398df6e80123197b4
Traunin <traunin5@gmail.com>
2024-11-28 16:32:00 +0300

    feat: about mock

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,15 @@
 <template>
-    О приложении
+    <p>Это приложение представляет собой аналитический инструмент для визуализации данных о просмотрах различных программ на каналах. Приложение может быть полезно для аналитиков, исследующих популярность программ.</p>
 </template>
+
+<style>
+p {
+    margin: 10px;
+    font-size: 24px;
+    font-family: "Roboto", sans-serif !important;
+    background: var(--md-sys-color-surface);
+    border-radius: 12px;
+    line-height: 1.5 !important;
+    padding: 20px !important;
+}
+</style>
