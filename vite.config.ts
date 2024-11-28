4418294b35fe3b1d3c8c6d40a88b94cf37089715
Traunin <traunin5@gmail.com>
2024-11-28 13:42:31 +0300

    init

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+
+  server: {
+    port: 80
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
