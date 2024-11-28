9414ed4c72761f3336ed9de82623595f3a662dc0
Traunin <traunin5@gmail.com>
2024-11-28 16:31:37 +0300

    feat: range picker setup

---
+++ src/components/RangePicker.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="range-picker">
+    <div class="header">
+        <h3>Время</h3>
+        <p>Выберите фильтры по времени</p>
+    </div>
+</div>    
+</template>
+
+<style scoped>
+h3 {
+    font-family: "Roboto", sans-serif !important;
+}
+.range-picker {
+    background: var(--md-sys-color-surface)
+}
+</style>
