8a00e10be0fd98d14932a1efe89b84d6af106be8
Traunin <traunin5@gmail.com>
2024-11-28 16:32:12 +0300

    feat: help mock

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,3 +1,15 @@
 <template>
-    Помошь
+    <p>Возникли вопросы? Пишите по адресу <a href="mailto:webmaster@example.com">mailtest@test.com</a></p>
 </template>
+
+<style>
+p {
+    margin: 10px;
+    font-size: 24px;
+    font-family: "Roboto", sans-serif !important;
+    background: var(--md-sys-color-surface);
+    border-radius: 12px;
+    line-height: 1.5 !important;
+    padding: 20px !important;
+}
+</style>
