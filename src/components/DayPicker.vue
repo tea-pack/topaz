ec351c0f959f7b6d384f01ef4f74f6958d6f4240
Traunin <traunin5@gmail.com>
2024-11-29 10:47:59 +0300

    feat: day picker component

--- src/components/DayPicker.vue
+++ src/components/DayPicker.vue
@@ -1,1 +1,39 @@
-<template>Пн Вт Ср Чт</template>
+<template>
+    <div class="days">
+        <day-button
+            v-for="(day, index) in days"
+            :label="day.label"
+            :toggled="day.toggled"
+            :key="day.label"
+            @click="toggleDay(index)"
+        ></day-button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import DayButton from '@/components/DayButton.vue';
+import {ref} from "vue"
+
+const days = ref([
+    { label: "Понедельник", toggled: false },
+    { label: "Вторник", toggled: false },
+    { label: "Среда", toggled: false },
+    { label: "Четверг", toggled: false },
+    { label: "Пятница", toggled: false },
+    { label: "Суббота", toggled: false },
+    { label: "Воскресенье", toggled: false },
+])
+
+function toggleDay(index: number) {
+    days.value[index].toggled =  !days.value[index].toggled
+    console.log(days.value[index].toggled)
+}
+</script>
+
+<style scoped>
+.days {
+    display: flex;
+    gap: 10px;
+    flex-wrap: wrap;
+}
+</style>
