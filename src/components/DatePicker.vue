988fb1d932db0fc04f0aafef2d24beb52f2e1d1c
Traunin <traunin5@gmail.com>
2024-11-29 10:47:21 +0300

    feat: date picker input

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,3 +1,40 @@
 <template>
-    Дата
+    <div class="picker">
+        <input type="text">
+        <div class="label">
+            {{ props.label }}
+        </div>
+    </div>
 </template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    label: string
+}>()
+</script>
+
+<style scoped>
+.picker {
+    position: relative;
+}
+
+.label {
+    position: absolute;
+    top: -5px;
+    left: 16px;
+    background: var(--md-sys-color-surface);
+    font-weight: 400;
+    font-size: 12px;
+}
+
+input {
+    padding: 16px;
+    border-radius: 4px;
+    background: var(--md-sys-color-surface);
+    border: 3px solid var(--md-sys-color-primary);
+    width: 100%;
+    font-weight: 400;
+    font-size: 16px;
+    color: var(--md-sys-color-on-surface);
+}
+</style>
