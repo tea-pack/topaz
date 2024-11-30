d9cc2a0edda44d96b5ca03fbc6b179ecb5c708be
Traunin <traunin5@gmail.com>
2024-11-30 10:53:10 +0300

    feat: comment out unused code

--- src/components/DayPicker.vue
+++ src/components/DayPicker.vue
@@ -3,22 +3,18 @@
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue"
+// import {ref} from "vue"
 
-const days = ref([
-    { label: "Понедельник", toggled: false },
-    { label: "Вторник", toggled: false },
-    { label: "Среда", toggled: false },
-    { label: "Четверг", toggled: false },
-    { label: "Пятница", toggled: false },
-    { label: "Суббота", toggled: false },
-    { label: "Воскресенье", toggled: false },
-])
+// const days = ref([
+//     { label: "Понедельник", toggled: false },
+//     { label: "Вторник", toggled: false },
+//     { label: "Среда", toggled: false },
+//     { label: "Четверг", toggled: false },
+//     { label: "Пятница", toggled: false },
+//     { label: "Суббота", toggled: false },
+//     { label: "Воскресенье", toggled: false },
+// ])
 
-function toggleDay(index: number) {
-    days.value[index].toggled =  !days.value[index].toggled
-    console.log(days.value[index].toggled)
-}
 </script>
 
 <style scoped>
